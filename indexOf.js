28b35cd0989c6b49a02e629ef2c21647742da0ab
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-01-28 22:55:06 +0600

    indexOf to get the index of any element

---
+++ indexOf.js
@@ -0,0 +1,10 @@
+const friends = ['balam', 'kalam', 'salam', 'gelam', 'pailam'];
+
+/**
+ * indexOf(x): to get the index of x in the array
+ * 
+ * ==> if x is not include in array then it give -1
+ */
+
+console.log(friends.indexOf('gelam'));
+console.log(friends.indexOf('moon'));

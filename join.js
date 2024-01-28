328863a99cd2a6c356e498c5d73f6e9365d7727b
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-01-28 23:01:56 +0600

    join('+') used to return an array as a string and element will separated by +

---
+++ join.js
@@ -0,0 +1,7 @@
+const nums = [1, 43, 4, 3]
+console.log(nums);
+
+// join('+'): returns an array as a string and element will separated by '+'
+
+console.log(nums.join('+'));
+console.log(nums.join('  '));

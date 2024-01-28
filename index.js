fd6cb9155b3042d4cd5899e8c7c6ceaf86e2fab3
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-01-28 22:37:41 +0600

    array-indexing and element update via array-indexing

---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+ * ----- indexing -----
+ * 
+ * ==> 0 indexing
+ * 
+ * ==> ith index element: array[i]
+ * 
+ * ==> 3 no element: index 2
+ * 
+ * ==> set or update element value by index
+ */
+
+let numbers = [1, 5, 66, 45, 65, 65, 88, 11, 45, 65, 65, 88, 15];
+
+console.log(numbers);
+console.log(numbers[2]);
+
+const fourth = numbers[3]
+console.log(fourth);
+
+// update
+numbers[3] = 4;
+console.log(numbers[3]);

ad4adabc5b4ea3f6be65baf04830c6ec2813ba87
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-01-28 22:27:21 +0600

    array declaretion

---
+++ array.js
@@ -0,0 +1,13 @@
+/**
+ * ------ ARRAY ------
+ * 1. , diye element gula boshabo
+ * 2. [] diye value gula wrap thakbe
+ */
+
+const numbers = [12, 22, 54, 65];
+
+const friends = ['Abul', 'Babul', 'Kabul', 'Dadul', 'Bulbul'];
+
+const passed = [true, false, true, false];
+
+const mixedArray = [12, 'Alahbad', true, 45.566];

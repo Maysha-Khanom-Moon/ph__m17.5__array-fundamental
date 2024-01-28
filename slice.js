1277d3283d7ed66924c45fb18f5a6fe730e0db42
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-01-28 23:13:11 +0600

    slice(x,y) func used to slice an array x-index element to y no element

---
+++ slice.js
@@ -0,0 +1,10 @@
+/**
+ * slice(x, y): x index element to y no element
+ * 
+ * ==> do not changed the original array
+ */
+
+let numbers = [1, 5, 66, 45, 65, 65, 88, 11, 45, 65, 65, 88, 15];
+
+let nums = numbers.slice(1, 4);
+console.log(nums);

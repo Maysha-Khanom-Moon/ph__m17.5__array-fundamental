dc2d6a4e39b1d8317048e6aeb4e7a166db051b95
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-01-28 23:08:09 +0600

    concat more than one array's element and make a new array

---
+++ concat.js
@@ -0,0 +1,10 @@
+const nums1 = [1, 43, 4, 3]
+const nums2 = [10, 430, 40, 30]
+
+
+// concat: to concat array's element and return a array
+const nums = nums1.concat(nums2);
+console.log(nums);
+
+const big = nums.concat(nums2, nums1);
+console.log(big);

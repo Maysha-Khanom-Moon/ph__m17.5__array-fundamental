a59b924861c57396fad973a7d08c689382f2c158
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-01-28 22:56:38 +0600

    Array.isArray() to check it is array or not

---
+++ isArray.js
@@ -0,0 +1,8 @@
+const friends = ['balam', 'kalam', 'salam', 'gelam', 'pailam'];
+
+const nums = [];
+const food = 'ros o gol l a'
+
+console.log(Array.isArray(friends));
+console.log(Array.isArray(nums));
+console.log(Array.isArray(food));
